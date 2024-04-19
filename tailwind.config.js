60ed6cd296771d31142e8e49b4175cc423644ab3
Just0neMoment <dnxks1222@gmail.com>
2024-04-19 20:25:37 +0900

    프로젝트 셋팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
+const { nextui } = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: ["./src//*.{js,jsx,ts,tsx}", ["./index.html"]],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#eeeeee",
+        primary: {
+          DEFAULT: "#11ddaa",
+          foreground: "#eeeeee",
+        },
+      },
+    },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [
+    nextui({
+      addCommonColors: true,
+    }),
+  ],
 };
