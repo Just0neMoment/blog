04c973c409dd9c0d327d4c3911e2918d7487b111
Just0neMoment <dnxks1222@gmail.com>
2024-04-19 09:54:29 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-[#11ddaa]">Hello World!</h1>
+    </>
+  );
+}
+
+export default App;
